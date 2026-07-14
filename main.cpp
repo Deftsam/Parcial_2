14d033146a0709a67cc43559c0b6f0942957e2e9
Juan Manuel Cadena <145808763+JuanCadenaUV@users.noreply.github.com>
2026-07-14 18:44:13 -0500

    Se agrega main.cpp que incluye lo pedido en (HU04)

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "Controlador.h"
+
+int main() {
+    Controlador controlador; // el Controlador carga los datos de prueba en su constructor
+    controlador.iniciar();   // main.cpp SOLO instancia y llama iniciar() (HU04)
+    return 0;
+}
